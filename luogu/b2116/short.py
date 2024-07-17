8425dc6689ccf443c68a3a80699b12a8b377c7df
eawlot3000 <eawlot3000@gmail.com>
2024-07-16 23:59:11 -0400

    update b2116 and more

---
+++ luogu/b2116/short.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+d = {'x':'a', 'y':'b', 'z':'c', 'X':'A', 'Y':'B', 'Z':'C'}
+a = ''.join([i.lower() if i.isupper() else i.upper() for i in str(input())])[::-1]
+
+# Create a list from the generator expression and join it to a single string
+result = ''.join(d[i] if i in d else chr(ord(i)+3) for i in a)
+print(result)
+
+# this is wrong!
+#print(d[i] if i in d else chr(ord(i)+3) for i in a)
+
