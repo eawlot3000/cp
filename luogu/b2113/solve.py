6c273ff3084711ab01dba2a85b4e2e124f9bc44a
eawlot3000 <eawlot3000@gmail.com>
2024-07-16 21:38:23 -0400

    added new b2113 solution

--- luogu/b2113/solve.py
+++ luogu/b2113/solve.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-
-s = str(input())
-for i in s:
-  i = str(i)
-  print(chr(i))
-
-
-
+a = str(input())
+for i in range(1, len(a)):
+  o = int(a[i]) + int(a[i-1])
+  print(chr(o+96), end='')
+  #print(o)
+print(chr(int(a[0])+int(a[-1])+96))
 
 
+'''
+a = 'e' 
+b = 101
+print(ord(a)-96)
+print(chr(b))
 
+
+a = 5 + 96
+print(chr(a))
+'''
