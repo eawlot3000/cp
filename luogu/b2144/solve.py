1d9c521fd3a1f425fed63db51de3287909493fd9
eawlot3000 <eawlot3000@gmail.com>
2024-07-27 23:59:22 -0400

    added b2144

---
+++ luogu/b2144/solve.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def ackermann(m, n):
+  if m == 0:
+    return n + 1
+  elif m > 0 and n == 0:
+    return ackermann(m - 1, 1)
+  elif m > 0 and n > 0:
+    return ackermann(m - 1, ackermann(m, n - 1))
+
+m, n = map(int, input().split())
+print(ackermann(m, n))
