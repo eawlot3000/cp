64fefd8ef39357636aeb08f5267153c2e1dcb1c2
eawlot3000 <eawlot3000@gmail.com>
2024-07-28 23:26:49 -0400

    added b2135

---
+++ luogu/b2135/solve.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+s = input().strip()
+a = input().strip()
+b = input().strip()
+print(s.replace(a, b))
+
+''' worked in one line:
+print(input().strip().replace(input().strip(), input().strip()))
+'''
